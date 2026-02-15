9441e26f80c239998f175196d9172094e16b2a2c
Julien BOUTOT~ <julien.boutot@epitech.eu>
2026-02-15 23:24:58 +0100

    [F] Circle's shape to look less like a rectangle

--- src/board/players_patterns.c
+++ src/board/players_patterns.c
@@ -32,18 +32,18 @@ static wchar_t **circle_pattern(wchar_t **pattern, int width, int height)
         pattern[0][width / 2 + 1 - 1] = L'O';
         return pattern;
     }
-    for (int circle_up = 0; circle_up < width; circle_up++)
+    for (int circle_up = 1; circle_up + 1 < width; circle_up++)
         pattern[0][circle_up] = L'─';
-    for (int circle_down = 0; circle_down < width; circle_down++)
+    for (int circle_down = 1; circle_down + 1 < width; circle_down++)
         pattern[height - 1][circle_down] = L'─';
     for (int circle_left = 0; circle_left < height; circle_left++)
-        pattern[circle_left][0] = L'│';
-    for (int circle_right = 0; circle_right < height; circle_right++)
-        pattern[circle_right][width - 1] = L'│';
-    pattern[0][0] = L'╭';
-    pattern[0][width - 1] = L'╮';
-    pattern[height - 1][0] = L'╰';
-    pattern[height - 1][width - 1] = L'╯';
+        pattern[circle_left][1] = L'│';
+    for (int circle_right = 1; circle_right < height; circle_right++)
+        pattern[circle_right][width - 2] = L'│';
+    pattern[0][1] = L'╭';
+    pattern[0][width - 2] = L'╮';
+    pattern[height - 1][1] = L'╰';
+    pattern[height - 1][width - 2] = L'╯';
     return pattern;
 }
 
